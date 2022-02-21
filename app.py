43e01c79658241fdbf3d4afd0628c48bff92f0f1
nanorohan <85023589+nanorohan@users.noreply.github.com>
2022-02-21 14:32:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -111,9 +111,7 @@ def convert_df(df):
 def main():
 	
 	html_photo = """
-	
-	<body>
-  	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 	<style> 
     	body{
@@ -155,7 +153,6 @@ def main():
 <br>
 	<p id="rcorners1" >Client Defaulter Tendency Predictor</p>
      
-</body>	
 	"""
 	st.markdown(html_photo,unsafe_allow_html=True)	
 	header_pic = Image.open('loan.jpg')
